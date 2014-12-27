a4207adcf5abaebacd146c81e29bf64eabbd919f
WANG Jiaqi <mine4ghost@gmail.com>
2014-12-28 00:47:15 +0800

    First Commit

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by 王枷淇 on 14/12/28.
+//  Copyright (c) 2014年 王枷淇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by 王枷淇 on 14/12/28.
+//  Copyright (c) 2014年 王枷淇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
